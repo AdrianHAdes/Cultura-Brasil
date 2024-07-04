815b56afc2e72638e98d66a293c912361ffbaa32
AdrianHAdes <santos.adrian03@escola.pr.gov.br>
2024-07-04 15:02:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Carnaval é uma festa culturalmente significativa no Brasil?",
+        alternativas: ["Sim", "Não"],
+    },
+    {
+        enunciado: "A Capoeira é uma forma de expressão cultural afro-brasileira?",
+        alternativas: ["Sim", "Não"],
+    },
+    {
+        enunciado: " samba é um gênero musical tradicionalmente brasileiro?",
+        alternativas: ["Sim", "Não"],
+    },
+];
+
+let atual = 0;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual < perguntas.length) {
+        const perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraAlternativas(perguntaAtual.alternativas);
+    } else {
+        caixaPerguntas.textContent = '';
+        caixaAlternativas.innerHTML = '';
+        textoResultado.textContent = `Você respondeu todas as perguntas! Sua história: ${historiaFinal}`;
+    }
+}
+
+function mostraAlternativas(alternativas) {
+    caixaAlternativas.innerHTML = ''; // Limpa as alternativas anteriores
+    alternativas.forEach(alternativa => {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        botaoAlternativa.addEventListener('click', () => {
+            respostaSelecionada(alternativa);
+        });
+        caixaAlternativas.appendChild(botaoAlternativa);
+    });
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    historiaFinal += opcaoSelecionada + ' ';
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
